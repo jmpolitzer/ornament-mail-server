052cbe26a87d5f8d6b5b5f4abad28e1107876a8b
Jay Politzer <jmpolitzer@gmail.com>
2018-05-15 13:42:19 -0500

    removes unnecesary jwt dep and code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import path from 'path';
 import http from 'http';
-import jwt from 'jsonwebtoken';
 
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 8080;
@@ -14,18 +13,6 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use((req, res, next) => {
-//   if(req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-//     jwt.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', (err, decode) => {
-//       if(err) req.user = undefined;
-//       req.user = decode;
-//       next();
-//     });
-//   } else {
-//     req.user = undefined;
-//     next();
-//   }
-// });
 
 require('./server/routes')(app);
 
